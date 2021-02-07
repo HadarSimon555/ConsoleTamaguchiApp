84463470662a74e07fcf0793af16100c5c129c0e
HadarSimon555 <77894680+HadarSimon555@users.noreply.github.com>
2021-02-07 16:56:09 +0200

    Add ActuonDTO

---
+++ UI/DataTransferObjects/ActionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleTamaguchiApp.DataTransferObjects
+{
+    public class ActionDTO
+    {
+        public int ActionId { get; set; }
+        public string ActionName { get; set; }
+        public int ActionTypeId { get; set; }
+        public ActionDTO() { }
+    }
+}
